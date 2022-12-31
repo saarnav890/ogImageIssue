a35da163612ba37a15936874bc186fe6f503c55a
Aarnav <63823257+saarnav890@users.noreply.github.com>
2022-12-31 12:40:52 -0600

    first commit

---
+++ app/[slug]/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+    return (
+      <>
+        <title key="RegTitle">Slug title</title>
+        <meta
+          property="og:image"
+          //this image does not load on the fb sharing debugger for dynamic routes
+          content="https://d1y8sb8igg2f8e.cloudfront.net/images/shutterstock_682265785_4.2e16d0ba.fill-1200x630.jpg"
+        />
+      </>
+    );
+  }
+  
---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+    return (
+      <>
+        <title key="RegTitle">Homepage Title</title>
+        <meta
+          property="og:image"
+          //this image does load on the fb sharing debugger
+          content="https://d1y8sb8igg2f8e.cloudfront.net/images/shutterstock_682265785_4.2e16d0ba.fill-1200x630.jpg"
+        />
+      </>
+    );
+  }
+  
